9cd3deba6170d3f9369dcd05ff955311e08af0e3
e7gx <abalgabo@gmail.com>
2024-07-09 17:30:20 +0300

    the lab has been solved 👋

--- google_clone/clone/views.py
+++ google_clone/clone/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpRequest
 
 # Create your views here.
-def search(request):
+def search(request:HttpRequest):
     return render(request, 'clone/search.html')
 
-def term(request):
+def term(request:HttpRequest):
     return render(request, 'clone/term.html')
