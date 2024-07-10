ae4e66eaccc6651763518b1a5e3c9b7effb113da
9twy <saeed.abdullah.alghamdi@outlook.sa>
2024-07-10 09:40:43 +0300

    terms of services page created.

--- googlePage/main/urls.py
+++ googlePage/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = "name"
 urlpatterns = [
-    path('',views.home_view,name="home_view")
+    path('',views.home_view,name="home_view"),
+    path("terms/services/",views.terms_of_services,name="term_view")
 ]
--- googlePage/main/views.py
+++ googlePage/main/views.py
@@ -4,5 +4,7 @@ from django.http import HttpRequest,HttpResponse
 # Create your views here.
 def home_view(request:HttpRequest):
     return render(request,"main/index.html")
+def terms_of_services(request:HttpRequest):
+    return render(request,"main/term.html")
 
-    
+
